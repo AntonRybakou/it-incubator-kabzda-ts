6fb36681e12fb4a4eaad82df08cd996fceb44e75
ToshaBY <anton.rybakou@gmail.com>
2022-05-02 11:15:50 +0300

    feat: uncontrolledrating component

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -9,49 +9,24 @@ export const UncontrolledRating: React.FC<UncontrolleRatingPropsType> = (props) 
 
     return (
         <div>
-            <Star selected={state > 0}/>
-            <button onClick={() => {
-                setState(1)
-            }}>1
-            </button>
-            <Star selected={state > 1}/>
-            <button onClick={() => {
-                setState(2)
-            }}>2
-            </button>
-            <Star selected={state > 2}/>
-            <button onClick={() => {
-                setState(3)
-            }}>3
-            </button>
-            <Star selected={state > 3}/>
-            <button onClick={() => {
-                setState(4)
-            }}>4
-            </button>
-            <Star selected={state > 4}/>
-            <button onClick={() => {
-                setState(5)
-            }}>5
-            </button>
+            <Star selected={state > 0} setState={setState} value={1}/>
+            <Star selected={state > 1} setState={setState} value={2}/>
+            <Star selected={state > 2} setState={setState} value={3}/>
+            <Star selected={state > 3} setState={setState} value={4}/>
+            <Star selected={state > 4} setState={setState} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean;
+    value: 0 | 1 | 2 | 3 | 4 | 5;
+    setState: (newState: 0 | 1 | 2 | 3 | 4 | 5) => void;
 }
 
 function Star(props: StarPropsType) {
     console.log("Star rendering")
-
-    props.selected ? <span><b>star </b></span> : <span>star </span>
-
-    // if (props.selected) {
-    //     return <span><b>star </b></span>
-    // } else {
-    //     return (
-    //         <span>star </span>
-    //     )
-    // }
+    return <span onClick={ () => {props.setState(props.value)} }>
+        {props.selected ? <b>star </b> : `star `}
+    </span>
 }
